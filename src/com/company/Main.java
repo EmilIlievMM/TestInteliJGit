39634bd868e3bc8e4125cfb9b96e89ed9dac884b
Emil Iliev <emil.iliev@mentormate.com>
2022-04-05 18:29:19 +0300

    Test Java and Git

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Item 1
+        // Item 2
+        // Item 3
+        // Item 4
+        // Item 5
+    }
+}
